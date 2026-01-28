77ae50809a624191b541e91bd8307e055c7add3a
Sarvarbek-Erkinov <250421@newuu.uz>
2026-01-28 13:46:42 +0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,98 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        //Problem 3
+//        double width = 4.5, height = 7.9;
+//        double area = width * height;
+//        double peri = 2*(width + height);
+//        System.out.printf("Area: %2f\n", area);
+//        System.out.printf("Perimetr: %2f", peri);
+        //Problem 1
+//        System.out.println("Eshqulov Sanjar");
+//        System.out.println("Tashkent. Yakkasaroy. Naqsh.164");
+//        System.out.println("Postal: 110400");
+//        System.out.println("Country: Uzbekistan");
+        //Problem 2
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Please Enter your name: ");
+//        String name = input.nextLine();
+//        System.out.println("Hello " +name);
+        //Problem 4
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Please enter the radius: ");
+//        double radius = input.nextInt();
+//        input.nextLine();
+//        System.out.print("Please enter the length: ");
+//        double length = input.nextInt();
+//        input.nextLine();
+//        double area = radius * radius * Math.PI;
+//        double volume = area * length;
+//        System.out.printf("The Area is: %2f\n", area);
+//        System.out.printf("The Volume is: %2f", volume);
+        //Problem 5
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Please enter the length: ");
+//        double length = input.nextDouble();
+//        input.nextLine();
+//        double area = Math.sqrt(3)/4 * length * length;
+//        double vilume = area * length;
+//        System.out.printf("The area is: %.2f\n", area);
+//        System.out.printf("The volume is: %.2f\n", vilume);
+//
+//        input.close();
+        //Problem 6
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Please enter the cost: ");
+//        double cost = input.nextDouble();
+//        input.nextLine();
+//        double tip = cost * 0.18;
+//        double tax = cost * 0.12;
+//        double total = tip + tax;
+//        System.out.printf("The grand total: %.2f\n", total);
+//        System.out.printf("The tip: %.2f\n", tip);
+//        System.out.printf("The tax: %.2f\n", tax);
+        //Problem 7
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter the driving distance: ");
+//        double distance = input.nextDouble();
+//        input.nextLine();
+//        System.out.print("Enter miles per gallon: ");
+//        double milesPerGallon = input.nextDouble();
+//        input.nextLine();
+//        System.out.print("Enter price per gallon: ");
+//        double pricePerGallon = input.nextDouble();
+//        input.nextLine();
+//        double cost = distance / milesPerGallon * pricePerGallon;
+//
+//        System.out.printf("The cost of driving is: $%.2f", cost);
+        //Problem 8
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Please enter dour-digit number: ");
+//        int number = input.nextInt();
+//        input.nextLine();
+//        int sumOfDigits = number % 10;
+//        number = number / 10;
+//        sumOfDigits += number % 10;
+//        number /= 10;
+//        sumOfDigits += number% 10;
+//        number /= 10;
+//        sumOfDigits += number % 10;
+//        System.out.print("The sum of digits: " + sumOfDigits);
+        //Problem 9
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Please enter three numbers: ");
+//        int numOne = input.nextInt();
+//        input.nextLine();
+//        int numTwo = input.nextInt();
+//        input.nextLine();
+//        int numThree = input.nextInt();
+//        input.nextLine();
+//        int maxNum = Math.max(Math.max(numOne, numTwo), numThree);
+//        int minNum = Math.min(Math.min(numOne, numTwo), numThree);
+//        int middleNum = numOne + numTwo + numThree - maxNum - minNum;
+//        System.out.println(minNum);
+//        System.out.println(middleNum);
+//        System.out.println(maxNum);
+    }
+}
